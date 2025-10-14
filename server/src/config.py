2364a4271ab46f3352bfee45984be0a66be38131
Paarth Batra <138647078+hydro-7@users.noreply.github.com>
2025-10-14 23:11:15 +0530

    Update config.py

--- server/src/config.py
+++ server/src/config.py
@@ -16,9 +16,9 @@ class Secrets(BaseSettings):
     MAIL_PASSWORD: str = "password"
     MAIL_USERNAME: str = "test@example.com"
     APPS_SCRIPT_URL: str = "https://script.google.com/macros/s/abc/exec"
-    PUSHER_APP_ID: str
-    PUSHER_KEY: str
-    PUSHER_SECRET: str
+    PUSHER_APP_ID:str = "212345"
+    PUSHER_KEY:str = "xbjcvjfcvjcbaxvbcjvb"
+    PUSHER_SECRET:str = "bcjzvcjzvxcjvbsjhcbv"
 
     class Config:
         env_file = ".env"
