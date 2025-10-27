abfdfb640aa917cefbb57abb1db510f61eee8e47
SiddharthKarmokar <siddkarmokar@gmail.com>
2025-10-27 13:57:01 +0530

    added logs to mail

--- server/src/services/utils.py
+++ server/src/services/utils.py
@@ -84,6 +84,7 @@ def queue_email_task(
     """
     if isinstance(background_tasks, BackgroundTasks):
         background_tasks.add_task(send_email_to_student, email, subject, body)
+        logger.info(f"Send mail to {email}")
 
 
 async def send_email(to_email: str, subject: str, body: str) -> None:
